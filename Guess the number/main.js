dd70a92b6d70f7227675a4305e2144ec7dbd60d5
LavrentiEmirashvili <lavrentiemirashvili@gmail.com>
2024-03-06 18:15:20 +0400

    updated main.js in guess the number

--- Guess the number/main.js
+++ Guess the number/main.js
@@ -1,7 +1,9 @@
 let attempts = Number(
   prompt("Enter the number of attempts (leave blank for 10 attempts):")
 );
-attempts = isNaN(attempts) || attempts === 0 ? 10 : attempts;
+if (isNaN(attempts) || attempts === 0) {
+  attempts = 10;
+}
 const randomNumber = Math.floor(Math.random() * 1000) + 1;
 let guess;
 for (let i = 0; i < attempts; i++) {
